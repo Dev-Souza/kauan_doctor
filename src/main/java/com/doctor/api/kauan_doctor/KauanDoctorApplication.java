2c5b1e9e17403e902d480e47d683bed901b1f600
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-05 01:23:28 -0300

    feat: initializr project the hospital

---
+++ src/main/java/com/doctor/api/kauan_doctor/KauanDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.doctor.api.kauan_doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KauanDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KauanDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctor/api/kauan_doctor/KauanDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctor.api.kauan_doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KauanDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
