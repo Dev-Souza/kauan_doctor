290f99f30b0dab0cd5d087ecd403eb5276525618
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-11 20:35:58 -0300

    feat: criacao de novos atributos e o enum de status

--- src/main/java/com/doctor/api/kauan_doctor/model/consulta/ConsultaModel.java
+++ src/main/java/com/doctor/api/kauan_doctor/model/consulta/ConsultaModel.java
@@ -7,9 +7,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 @Table(name = "consulta")
 @Getter
 @Setter
@@ -33,4 +30,10 @@ public class ConsultaModel {
     @OneToOne
     @JoinColumn(name = "agenda_id", unique = true)
     private AgendaModel agenda;
+
+    private String observacao;
+
+    @Enumerated(EnumType.STRING)
+    private StatusConsultaEnum status;
+
 }
---
+++ src/main/java/com/doctor/api/kauan_doctor/model/consulta/StatusConsultaEnum.java
@@ -0,0 +1,8 @@
+package com.doctor.api.kauan_doctor.model.consulta;
+
+public enum StatusConsultaEnum {
+    MARCADA,
+    CONCLUIDA,
+    CANCELADA,
+    AUSENTE
+}
