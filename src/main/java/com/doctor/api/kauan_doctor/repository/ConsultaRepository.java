d251a8b7f78dd32d5e309d0bf8fbec8a83243259
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-13 10:27:36 -0300

    feat: function repository para pegar o histórico de consultas por status

--- src/main/java/com/doctor/api/kauan_doctor/repository/ConsultaRepository.java
+++ src/main/java/com/doctor/api/kauan_doctor/repository/ConsultaRepository.java
@@ -1,6 +1,7 @@
 package com.doctor.api.kauan_doctor.repository;
 
 import com.doctor.api.kauan_doctor.model.consulta.ConsultaModel;
+import com.doctor.api.kauan_doctor.model.consulta.StatusConsultaEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,7 @@ public interface ConsultaRepository extends JpaRepository<ConsultaModel, Long> {
 
     @Query(value = "SELECT * FROM consulta WHERE medico_id = :medico_id", nativeQuery = true)
     List<ConsultaModel> listaConsultasDeUmMedico(@Param("medico_id") Long medicoId);
+
+    @Query(value = "SELECT * FROM consulta WHERE paciente_id = :paciente_id AND status = :status", nativeQuery = true)
+    List<ConsultaModel> consultasPacientePorStatus(@Param("paciente_id") Long paciente_id, @Param("status") StatusConsultaEnum status);
 }
