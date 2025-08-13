4146cf58a8f38a7839fdb762acf5ab4a7b59f349
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-13 08:49:52 -0300

    feat: function no repository para trazer todas as consultas de um mmedico

--- src/main/java/com/doctor/api/kauan_doctor/repository/ConsultaRepository.java
+++ src/main/java/com/doctor/api/kauan_doctor/repository/ConsultaRepository.java
@@ -2,6 +2,13 @@ package com.doctor.api.kauan_doctor.repository;
 
 import com.doctor.api.kauan_doctor.model.consulta.ConsultaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ConsultaRepository extends JpaRepository<ConsultaModel, Long> {
+
+    @Query(value = "SELECT * FROM consulta WHERE medico_id = :medico_id", nativeQuery = true)
+    List<ConsultaModel> listaConsultasDeUmMedico(@Param("medico_id") Long medicoId);
 }
