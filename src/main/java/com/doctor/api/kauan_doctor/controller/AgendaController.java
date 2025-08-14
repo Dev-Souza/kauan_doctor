d9664ab779b04941c8234530c762dbeb1e59c565
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-14 09:48:02 -0300

    feat: controller de agenda

--- src/main/java/com/doctor/api/kauan_doctor/controller/AgendaController.java
+++ src/main/java/com/doctor/api/kauan_doctor/controller/AgendaController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Agendas")
@@ -24,4 +23,25 @@ public class AgendaController {
     @PreAuthorize("hasRole('MEDICO')")
     @PostMapping
     public ResponseEntity<AgendaResponseDTO> createAgenda (@RequestBody AgendaRequestDTO agendaRequestDTO) {return agendaService.createAgenda(agendaRequestDTO);}
+
+    // VER DISPONIBILIDADE DA AGENDA DE UM MÉDICO
+    @PreAuthorize("hasRole('MEDICO')")
+    @GetMapping("diponibilidade/medico/{medico_id}")
+    public ResponseEntity<List<AgendaResponseDTO>> listaDeAgendaDisponivel(@PathVariable("medico_id") Long medico_id){return agendaService.findAllAgendasDeUmMedico(medico_id);}
+
+    // GET ALL AGENDAS
+    @GetMapping
+    public ResponseEntity<List<AgendaResponseDTO>> getAllAgendas() {return agendaService.getllAgendas();}
+
+    // GET AGENDA BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<AgendaResponseDTO> getAgendaById(@PathVariable("id") Long id) {return agendaService.getAgendaById(id);}
+
+    // UPDATE AGENDA
+    @PutMapping("/{id}")
+    public ResponseEntity<AgendaResponseDTO> updateAgenda(@PathVariable Long id, @RequestBody AgendaRequestDTO dto) {return agendaService.updateAgenda(id, dto);}
+
+    // DELETE AGENDA
+    @DeleteMapping("{id}")
+    public ResponseEntity<AgendaResponseDTO> deleteAgenda(@PathVariable Long id) {return agendaService.deleteAgenda(id);}
 }
