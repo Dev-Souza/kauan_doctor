79b816e42eac7d5d756c01bade85063cba6302cd
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-14 07:51:49 -0300

    feat: rotas na controller de paciente configuradas

--- src/main/java/com/doctor/api/kauan_doctor/controller/PacienteController.java
+++ src/main/java/com/doctor/api/kauan_doctor/controller/PacienteController.java
@@ -6,10 +6,9 @@ import com.doctor.api.kauan_doctor.service.PacienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,4 +21,20 @@ public class PacienteController {
     // CREATE PACIENTE
     @PostMapping
     public ResponseEntity<PacienteResponseDTO> createPaciente (@RequestBody PacienteRequestDTO pacienteRequestDTO) {return pacienteService.createPaciente(pacienteRequestDTO);}
+
+    // GET ALL PACIENTES
+    @GetMapping
+    public ResponseEntity<List<PacienteResponseDTO>> getAllPacientes() {return pacienteService.getAllPacientes();}
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteResponseDTO> getPacienteById(@PathVariable("id") Long id) {return pacienteService.getPacienteById(id);}
+
+    // UPDATE PACIENTE
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteResponseDTO> updatePaciente(@PathVariable("id") Long id, @RequestBody PacienteRequestDTO dto){return pacienteService.updatePaciente(id, dto);}
+
+    // DELETE PACIENTE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PacienteResponseDTO> deletePaciente(@PathVariable("id") Long id) {return pacienteService.deletePaciente(id);}
 }
