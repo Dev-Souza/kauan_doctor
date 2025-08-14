057d15f40504db42fb62fab8efdfddc597650815
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-14 08:52:08 -0300

    feat: controller de medico

--- src/main/java/com/doctor/api/kauan_doctor/controller/MedicoController.java
+++ src/main/java/com/doctor/api/kauan_doctor/controller/MedicoController.java
@@ -6,10 +6,9 @@ import com.doctor.api.kauan_doctor.service.MedicoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,4 +22,19 @@ public class MedicoController {
     @PostMapping
     public ResponseEntity<MedicoResponseDTO> createMedico (@RequestBody MedicoRequestDTO medicoRequestDTO) {return medicoService.createMedico(medicoRequestDTO);}
 
+    // GET ALL MEDICOS
+    @GetMapping
+    public ResponseEntity<List<MedicoResponseDTO>> getAllMedicos () {return medicoService.getAllMedicos();}
+
+    // GET BY ID MÉDICO
+    @GetMapping("/{id}")
+    public ResponseEntity<MedicoResponseDTO> getById(@PathVariable Long id) {return medicoService.getMedicoById(id);}
+
+    // UPDATE MÉDICO
+    @PutMapping("/{id}")
+    public ResponseEntity<MedicoResponseDTO> updateMedico(@PathVariable Long id, @RequestBody MedicoRequestDTO dto) {return medicoService.updateMedico(id, dto);}
+
+    // DELETE MÉDICO
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MedicoResponseDTO> deleteMedico(@PathVariable Long id) {return medicoService.deleteMedico(id);}
 }
