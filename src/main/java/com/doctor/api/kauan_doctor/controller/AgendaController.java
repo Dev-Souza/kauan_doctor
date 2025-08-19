c3b67a60960628b6be0dda5b9ea20e98775a05a9
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-19 10:34:34 -0300

    fix: name request mapping in agenda

--- src/main/java/com/doctor/api/kauan_doctor/controller/AgendaController.java
+++ src/main/java/com/doctor/api/kauan_doctor/controller/AgendaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Agendas")
+@RequestMapping("/agendas")
 @RequiredArgsConstructor
 public class AgendaController {
 
