44f17f841d537827481ef22cce3ff59f5f4835f0
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-20 12:50:54 -0300

    feat: funcao de alterar estado do status

--- src/main/java/com/doctor/api/kauan_doctor/repository/AgendaRepository.java
+++ src/main/java/com/doctor/api/kauan_doctor/repository/AgendaRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 public interface AgendaRepository extends JpaRepository<AgendaModel, Long> {
 
-    @Query(value = "SELECT * FROM agenda WHERE medico_id = :medico_id AND status = 'LIVRE'", nativeQuery = true)
+    @Query(value = "SELECT * FROM agenda WHERE medico_id = :medico_id AND status_agenda = 'LIVRE'", nativeQuery = true)
     List<AgendaModel> findAllAgenda(@Param("medico_id") Long medico_id);
+
+    @Query(value = "UPDATE agenda SET status_agenda = 'RESERVADA' WHERE id = :idAgenda", nativeQuery = true)
+    AgendaModel updateStatusAgenda(@Param("idAgenda") Long idAgenda);
 }
