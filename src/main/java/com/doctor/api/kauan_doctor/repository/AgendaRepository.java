a767ceb325c0e88a0864ad49e78f39af82f56280
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-20 13:00:12 -0300

    fix: mudando a funcao de alterar status Ãpara void

--- src/main/java/com/doctor/api/kauan_doctor/repository/AgendaRepository.java
+++ src/main/java/com/doctor/api/kauan_doctor/repository/AgendaRepository.java
@@ -1,7 +1,9 @@
 package com.doctor.api.kauan_doctor.repository;
 
 import com.doctor.api.kauan_doctor.model.agenda.AgendaModel;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,6 +14,8 @@ public interface AgendaRepository extends JpaRepository<AgendaModel, Long> {
     @Query(value = "SELECT * FROM agenda WHERE medico_id = :medico_id AND status_agenda = 'LIVRE'", nativeQuery = true)
     List<AgendaModel> findAllAgenda(@Param("medico_id") Long medico_id);
 
+    @Transactional
+    @Modifying
     @Query(value = "UPDATE agenda SET status_agenda = 'RESERVADA' WHERE id = :idAgenda", nativeQuery = true)
-    AgendaModel updateStatusAgenda(@Param("idAgenda") Long idAgenda);
+    void updateStatusAgenda(@Param("idAgenda") Long idAgenda);
 }
